bab6d8909dbe7c0c418c6313aebedaa977f7c550
Darsh <darshlin714@gmail.com>
2019-09-09 19:31:33 -0700

    changed to exact static path

--- conf.py
+++ conf.py
@@ -55,7 +55,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # # Add any paths that contain custom static files (such as style sheets) here,
 # # relative to this directory. They are copied after the builtin static files,
 # # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['html/_static']
 # html_theme_path = ['sphinx_bootstrap_theme.get_html_theme_path()']
 
 # html_theme_path = guzzle_sphinx_theme.html_theme_path()
